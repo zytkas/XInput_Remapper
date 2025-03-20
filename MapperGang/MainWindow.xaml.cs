169202da365fef8945f0934cf5d9eac0845d4913
zytka <danilazdanov@gmail.com>
2025-03-20 16:58:02 +0000

    Добавьте файлы проекта.

---
+++ MapperGang/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿
+namespace MapperGang
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow 
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
